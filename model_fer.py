534aa4daf930a0a0ce139c05a545141ceb5de053
jacobsanosh <jacobsanosh@gmail.com>
2024-03-31 19:23:03 +0530

    updating  fer model again

--- model_fer.py
+++ model_fer.py
@@ -1,73 +1,163 @@
-import numpy as np
-import pandas as pd
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import os
+#for dirname, _, filenames in os.walk('/kaggle/input'):
+    #for filename in filenames:
+        #print(os.path.join(dirname, filename))
 import matplotlib.pyplot as plt
 import seaborn as sns
 import tensorflow as tf
 import keras
 from keras.preprocessing import image
 from keras.models import Sequential
-from keras.layers import Conv2D, MaxPool2D, Flatten, Dense, Dropout, BatchNormalization
+from keras.layers import Conv2D, MaxPool2D, Flatten,Dense,Dropout,BatchNormalization
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import cv2
+from tensorflow.keras.applications import VGG16, InceptionResNetV2
+from keras import regularizers
+from tensorflow.keras.optimizers import Adam,RMSprop,SGD,Adamax
 
-# Define directories
-train_dir = "images/train"
-test_dir = "images/test"
 
-img_size = 48
+# In[2]:
 
-# Data Augmentation
-train_datagen = ImageDataGenerator(
-    width_shift_range=0.1,
-    height_shift_range=0.1,
-    horizontal_flip=True,
-    rescale=1./255,
-    validation_split=0.2
-)
 
-validation_datagen = ImageDataGenerator(
-    rescale=1./255,
-    validation_split=0.2
-)
+train_dir = "../input/emotion-detection-fer/train" #passing the path with training images
+test_dir = "../input/emotion-detection-fer/test"   #passing the path with testing images
 
-# Flow images from directories with augmentation
-train_generator = train_datagen.flow_from_directory(
-    directory=train_dir,
-    target_size=(img_size, img_size),
-    batch_size=32,
-    color_mode="grayscale",
-    class_mode="categorical",
-    subset="training"
-)
 
-validation_generator = validation_datagen.flow_from_directory(
-    directory=test_dir,
-    target_size=(img_size, img_size),
-    batch_size=32,
-    color_mode="grayscale",
-    class_mode="categorical",
-    subset="validation"
-)
+# In[3]:
 
-# Define CNN model
+
+img_size = 48 #original size of the image
+
+
+# In[4]:
+
+
+"""
+Data Augmentation
+--------------------------
+rotation_range = rotates the image with the amount of degrees we provide
+width_shift_range = shifts the image randomly to the right or left along the width of the image
+height_shift range = shifts image randomly to up or below along the height of the image
+horizontal_flip = flips the image horizontally
+rescale = to scale down the pizel values in our image between 0 and 1
+zoom_range = applies random zoom to our object
+validation_split = reserves some images to be used for validation purpose
+"""
+
+train_datagen = ImageDataGenerator(#rotation_range = 180,
+                                         width_shift_range = 0.1,
+                                         height_shift_range = 0.1,
+                                         horizontal_flip = True,
+                                         rescale = 1./255,
+                                         #zoom_range = 0.2,
+                                         validation_split = 0.2
+                                        )
+validation_datagen = ImageDataGenerator(rescale = 1./255,
+                                         validation_split = 0.2)
+
+
+# In[5]:
+
+
+"""
+Applying data augmentation to the images as we read 
+them from their respectivve directories
+"""
+train_generator = train_datagen.flow_from_directory(directory = train_dir,
+                                                    target_size = (img_size,img_size),
+                                                    batch_size = 64,
+                                                    color_mode = "grayscale",
+                                                    class_mode = "categorical",
+                                                    subset = "training"
+                                                   )
+validation_generator = validation_datagen.flow_from_directory( directory = test_dir,
+                                                              target_size = (img_size,img_size),
+                                                              batch_size = 64,
+                                                              color_mode = "grayscale",
+                                                              class_mode = "categorical",
+                                                              subset = "validation"
+                                                             )
+
+
+# In[6]:
+
+
+"""
+Modeling
+
+
 model = Sequential()
+model.add(Conv2D(filters = 64,kernel_size = (3,3),padding = 'same',activation = 'relu',input_shape=(img_size,img_size,1)))
+model.add(MaxPool2D(pool_size = 2,strides = 2))
+model.add(BatchNormalization())
 
-model.add(Conv2D(32, kernel_size=(3, 3), padding='same', activation='relu', input_shape=(img_size, img_size, 1)))
+model.add(Conv2D(filters = 128,kernel_size = (3,3),padding = 'same',activation = 'relu'))
+model.add(MaxPool2D(pool_size = 2,strides = 2))
+model.add(BatchNormalization())
+model.add(Dropout(0.25))
+
+model.add(Conv2D(filters = 128,kernel_size = (3,3),padding = 'same',activation = 'relu'))
+model.add(MaxPool2D(pool_size = 2,strides = 2))
+model.add(BatchNormalization())
+model.add(Dropout(0.25))
+
+model.add(Conv2D(filters = 256,kernel_size = (3,3),padding = 'same',activation = 'relu'))
+model.add(MaxPool2D(pool_size = 2,strides = 2))
+model.add(BatchNormalization())
+
+model.add(Flatten())
+model.add(Dense(units = 128,activation = 'relu',kernel_initializer='he_normal'))
+model.add(Dropout(0.25))
+model.add(Dense(units = 64,activation = 'relu',kernel_initializer='he_normal'))
+model.add(BatchNormalization())
+model.add(Dropout(0.25))
+model.add(Dense(units = 32,activation = 'relu',kernel_initializer='he_normal'))
+model.add(Dense(7,activation = 'softmax'))
+
+"""
+
+
+# In[7]:
+
+
+model = tf.keras.models.Sequential()
+
+model.add(Conv2D(32, kernel_size=(3, 3), padding='same', activation='relu', input_shape=(48, 48, 1)))
 model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
 model.add(BatchNormalization())
-model.add(MaxPool2D(pool_size=(2, 2), padding='same'))
+model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
 
 model.add(Conv2D(128, (5, 5), padding='same', activation='relu'))
 model.add(BatchNormalization())
-model.add(MaxPool2D(pool_size=(2, 2), padding='same'))
+model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
 
 model.add(Conv2D(512, (3, 3), padding='same', activation='relu', kernel_regularizer=regularizers.l2(0.01)))
 model.add(BatchNormalization())
-model.add(MaxPool2D(pool_size=(2, 2), padding='same'))
+model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
 
 model.add(Conv2D(512, (3, 3), padding='same', activation='relu', kernel_regularizer=regularizers.l2(0.01)))
 model.add(BatchNormalization())
-model.add(MaxPool2D(pool_size=(2, 2), padding='same'))
+model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
+
+model.add(Conv2D(512, (3, 3), padding='same', activation='relu', kernel_regularizer=regularizers.l2(0.01)))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
+
+model.add(Conv2D(512, (3, 3), padding='same', activation='relu', kernel_regularizer=regularizers.l2(0.01)))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
+
+model.add(Conv2D(512, (3, 3), padding='same', activation='relu', kernel_regularizer=regularizers.l2(0.01)))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
 
 model.add(Flatten())
 model.add(Dense(256, activation='relu'))
@@ -87,39 +177,111 @@ model.add(BatchNormalization())
 
 model.add(Dense(7, activation='softmax'))
 
-# Compile model
 model.compile(
     optimizer=Adam(lr=0.0001),
     loss='categorical_crossentropy',
     metrics=['accuracy']
 )
 
-# Train model
+
+# In[ ]:
+
+
 epochs = 60
+batch_size = 64
 
-history = model.fit(
-    x=train_generator,
-    epochs=epochs,
-    validation_data=validation_generator
-)
 
-# Plot training history
-plt.plot(history.history['accuracy'])
-plt.plot(history.history['val_accuracy'])
-plt.title('Model Accuracy')
-plt.ylabel('Accuracy')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Validation'], loc='upper left')
+# In[ ]:
+
+
+model.summary()
+
+
+# In[ ]:
+
+
+history = model.fit(x = train_generator,epochs = epochs,validation_data = validation_generator)
+
+
+# In[ ]:
+
+
+fig , ax = plt.subplots(1,2)
+train_acc = history.history['accuracy']
+train_loss = history.history['loss']
+fig.set_size_inches(12,4)
+
+ax[0].plot(history.history['accuracy'])
+ax[0].plot(history.history['val_accuracy'])
+ax[0].set_title('Training Accuracy vs Validation Accuracy')
+ax[0].set_ylabel('Accuracy')
+ax[0].set_xlabel('Epoch')
+ax[0].legend(['Train', 'Validation'], loc='upper left')
+
+ax[1].plot(history.history['loss'])
+ax[1].plot(history.history['val_loss'])
+ax[1].set_title('Training Loss vs Validation Loss')
+ax[1].set_ylabel('Loss')
+ax[1].set_xlabel('Epoch')
+ax[1].legend(['Train', 'Validation'], loc='upper left')
+
 plt.show()
 
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('Model Loss')
-plt.ylabel('Loss')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Validation'], loc='upper left')
-plt.show()
 
-# Save model and weights
+# In[ ]:
+
+
 model.save('model_optimal.h5')
+
+
+# In[ ]:
+
+
+img = image.load_img("../input/emotion-detection-fer/test/happy/im1021.png",target_size = (48,48),color_mode = "grayscale")
+img = np.array(img)
+plt.imshow(img)
+print(img.shape) #prints (48,48) that is the shape of our image
+
+
+# In[ ]:
+
+
+label_dict = {0:'Angry',1:'Disgust',2:'Fear',3:'Happy',4:'Neutral',5:'Sad',6:'Surprise'}
+
+
+# In[ ]:
+
+
+img = np.expand_dims(img,axis = 0) #makes image shape (1,48,48)
+img = img.reshape(1,48,48,1)
+result = model.predict(img)
+result = list(result[0])
+print(result)
+
+
+# In[ ]:
+
+
+img_index = result.index(max(result))
+print(label_dict[img_index])
+plt.show()
+
+
+# In[ ]:
+
+
+train_loss, train_acc = model.evaluate(train_generator)
+test_loss, test_acc   = model.evaluate(validation_generator)
+print("final train accuracy = {:.2f} , validation accuracy = {:.2f}".format(train_acc*100, test_acc*100))
+
+
+# In[ ]:
+
+
 model.save_weights('model_weights.h5')
+
+
+# In[ ]:
+
+
+
